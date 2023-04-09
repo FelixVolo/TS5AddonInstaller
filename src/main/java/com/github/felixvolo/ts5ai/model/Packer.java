7c03af9ec7087aeae5df792182e49d937556f696
Exopandora <mkonrad98@alice-dsl.net>
2023-04-09 18:25:35 +0200

    Download remote sources at install time

--- src/main/java/com/github/felixvolo/ts5ai/model/Packer.java
+++ src/main/java/com/github/felixvolo/ts5ai/model/Packer.java
@@ -1,7 +1,10 @@
 package com.github.felixvolo.ts5ai.model;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 import org.dom4j.Attribute;
 import org.dom4j.Document;
@@ -10,6 +13,7 @@ import org.dom4j.QName;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
+import com.github.felixvolo.ts5ai.util.IOUtils;
 import com.github.felixvolo.ts5ai.util.StringOutputStream;
 
 public class Packer {
@@ -38,10 +42,23 @@ public class Packer {
 	private static void packElement(Element element, IAddonSource source, String addonRoot, String sources) throws IOException {
 		if(NODE_SRCRIPT.equals(element.getName()) || NODE_STYLE.equals(element.getName())) {
 			Attribute src = element.attribute(SRC_ATTRIBUTE);
-			if(src != null && src.getValue().startsWith(sources)) {
-				String data = source.read(addonRoot + src.getValue());
-				element.remove(src);
-				element.setText(data);
+			if(src != null) {
+				if(src.getValue().startsWith(sources)) {
+					String data = source.read(addonRoot + src.getValue());
+					element.remove(src);
+					element.setText(data);
+				} else if(src.getValue().startsWith("https://") || src.getValue().startsWith("http://")) {
+					try {
+						URL url = new URL(src.getValue());
+						URLConnection connection = url.openConnection();
+						InputStream inputStream = connection.getInputStream();
+						String data = IOUtils.toString(inputStream);
+						element.remove(src);
+						element.setText(data);
+					} catch(Exception e) {
+						System.err.println("Could not download referened remote file \"" + src.getValue() + "\" (" + e.getMessage() + "). Keeping reference.");
+					}
+				}
 			}
 		}
 		// We want <script></script> and not <script/>
